e5b3468c604e12b57b8340eae7211c773034aedb
yoonkyung <kyung3098@naver.com>
2025-03-10 14:37:41 +0900

    feat: 유저 정보 수정 api 연동

---
+++ src/app/profile/edit/page.tsx
@@ -0,0 +1,11 @@
+import { EditProfile } from './_components/edit-profile';
+
+const Page = () => {
+  return (
+    <div className="px-[30px] m-auto w-full sm:w-[500px] h-screen ForGnbpaddingTop">
+      <EditProfile />
+    </div>
+  );
+};
+
+export default Page;
