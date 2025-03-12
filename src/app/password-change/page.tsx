db72872f0e4909710452e6080a6b274e0c6390e0
yoonkyung <kyung3098@naver.com>
2025-03-12 16:26:49 +0900

    feat:비밀번호 변경 로직

---
+++ src/app/password-change/page.tsx
@@ -0,0 +1,11 @@
+import { PasswordChangeForm } from './_components/password-change-form';
+
+const Page = () => {
+  return (
+    <div className="px-[30px] m-auto w-full sm:w-[500px] h-screen ForGnbpaddingTop">
+      <PasswordChangeForm />
+    </div>
+  );
+};
+
+export default Page;
